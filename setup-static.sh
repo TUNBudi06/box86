6b88a3a1ae69c09a7c49b1acb2e76fdc889c33fc
TUNBudi06 <70352065+TUNBudi06@users.noreply.github.com>
2022-06-12 16:52:10 +0700

    add zlib lib for gcc

--- setup-static.sh
+++ setup-static.sh
@@ -9,4 +9,4 @@ apt install cmake python3 build-essential gcc nano -y
 #installing static library
 apt install lib*stdc*dev-arm*-cross \
 lib*stdc*dev-i386-cross \
-libudns-dev -y
+libudns-dev libz-dev -y
